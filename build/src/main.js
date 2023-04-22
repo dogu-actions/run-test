454e1f4e0e42f1bf8a8e41e8e615f30571fe6587
oneofthezombies <hunhoekim@gmail.com>
2023-04-22 22:25:58 +0900

    publish to tag development

---
+++ build/src/main.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const action_kit_1 = require("@dogu-tech/action-kit");
+action_kit_1.ActionKit.run(async ({ options, logger, deviceHostClient, consoleActionClient }) => { });
+//# sourceMappingURL=main.js.map
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { ActionKit, HostPaths } from '@dogu-tech/action-kit';
+import { spawnSync } from 'child_process';
+import path from 'path';
+import fs from 'fs';
+
+ActionKit.run(async ({ options, logger, deviceHostClient, consoleActionClient }) => {});
