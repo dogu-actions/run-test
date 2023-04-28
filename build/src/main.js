a8a1e85e8db4f005cc02b4b1855062e1487221ee
oneofthezombies <hunhoekim@gmail.com>
2023-04-29 05:18:42 +0900

    Update main.js

--- build/src/main.js
+++ build/src/main.js
@@ -7,71 +7,76 @@ const action_kit_1 = require("@dogu-tech/action-kit");
 const child_process_1 = require("child_process");
 const fs_1 = __importDefault(require("fs"));
 const path_1 = __importDefault(require("path"));
-action_kit_1.ActionKit.run(async ({ options, logger, config, deviceHostClient, consoleActionClient }) => {
-    const { DOGU_ACTION_INPUTS, DOGU_DEVICE_WORKSPACE_ON_HOST_PATH, DOGU_PROJECT_ID, DOGU_LOG_LEVEL } = options;
+action_kit_1.ActionKit.run(async ({ options, logger, input, deviceHostClient }) => {
+    const { DOGU_LOG_LEVEL, DOGU_DEVICE_PROJECT_WORKSPACE_PATH, DOGU_RUN_TYPE } = options;
     logger.info('log level', { DOGU_LOG_LEVEL });
-    const validatedInputs = await (0, action_kit_1.transformAndValidate)(action_kit_1.RunTestInputs, JSON.parse(DOGU_ACTION_INPUTS));
-    const { script } = validatedInputs;
-    const paths = await deviceHostClient.getPaths();
-    const nodeBinPath = path_1.default.dirname(paths.common.node16);
-    const yarnPath = action_kit_1.HostPaths.yarnPath(paths.common.node16);
-    let deviceProjectGitPath = path_1.default.resolve(action_kit_1.HostPaths.deviceProjectGitPath(DOGU_DEVICE_WORKSPACE_ON_HOST_PATH, DOGU_PROJECT_ID));
-    async function loadUserLocalProjectPath() {
-        try {
-            const devConfig = await fs_1.default.promises.readFile('dev.config.json', {
-                encoding: 'utf8',
-            });
-            const parsed = JSON.parse(devConfig);
-            if (parsed.localUserProject.use) {
-                return parsed.localUserProject.path;
+    const script = input.get('script');
+    const pathMap = await deviceHostClient.getPathMap();
+    const { yarn } = pathMap.common;
+    let yarnPath = yarn;
+    let userProjectPath = path_1.default.resolve(action_kit_1.HostPaths.deviceProjectGitPath(DOGU_DEVICE_PROJECT_WORKSPACE_PATH));
+    const optionsConfig = await action_kit_1.OptionsConfig.load();
+    if (optionsConfig.get('localUserProject.use', false)) {
+        logger.info('Using local user project...');
+        async function findLocalUserProject() {
+            const searchPaths = optionsConfig.get('localUserProject.searchPaths', []);
+            for (const searchPath of searchPaths) {
+                const candidate = path_1.default.resolve(searchPath);
+                logger.info('Checking local user project path', { candidate });
+                const doguConfigPath = path_1.default.resolve(candidate, 'dogu.config.json');
+                const stat = await fs_1.default.promises.stat(doguConfigPath).catch(() => null);
+                if (stat) {
+                    return candidate;
+                }
             }
+            throw new Error(`Local user project not found in search paths: ${searchPaths.join(', ')}`);
         }
-        catch (error) {
-            logger.debug('dev.config.json failed', { error: (0, action_kit_1.errorify)(error) });
-        }
-        return '';
-    }
-    const localUserProjectPath = await loadUserLocalProjectPath();
-    if (localUserProjectPath) {
-        logger.info('Running locally');
-        deviceProjectGitPath = localUserProjectPath;
+        yarnPath = 'yarn';
+        userProjectPath = await findLocalUserProject();
     }
     else {
-        logger.info('Running on device host project');
+        logger.info('Using device user project...');
     }
-    function yarn(args) {
+    logger.info('User project path and yarn path', { userProjectPath, yarnPath });
+    function runYarn(args) {
         const command = yarnPath;
         logger.info(`Running command: ${command} ${args.join(' ')}`);
-        const env = {
-            ...process.env,
-            PATH: `${nodeBinPath}:${process.env.PATH}`,
-        };
-        logger.verbose('env', { env });
-        const cwd = deviceProjectGitPath;
-        logger.verbose('cwd', {
-            cwd,
-        });
         const result = (0, child_process_1.spawnSync)(command, args, {
             stdio: 'inherit',
-            env,
-            cwd,
+            cwd: userProjectPath,
         });
         if (result.status !== 0) {
             throw new Error(`Command failed: ${command} ${args.join(' ')}`);
         }
     }
-    const yarnLockPath = path_1.default.resolve(deviceProjectGitPath, 'yarn.lock');
-    const stat = await fs_1.default.promises.stat(yarnLockPath).catch(() => null);
-    if (!stat) {
-        logger.info('yarn.lock not found, create yarn.lock');
-        const handle = await fs_1.default.promises.open(yarnLockPath, 'w');
-        await handle.close();
-        logger.info('yarn.lock created');
+    if (!optionsConfig.get('localUserProject.use', false)) {
+        runYarn(['install']);
+        const packageJsonPath = path_1.default.resolve(userProjectPath, 'package.json');
+        const content = await fs_1.default.promises.readFile(packageJsonPath, 'utf8');
+        const packageJson = JSON.parse(content);
+        const doguDependencies = [];
+        if (packageJson.dependencies) {
+            for (const [key, value] of Object.entries(packageJson.dependencies)) {
+                if (key.startsWith('@dogu-tech/')) {
+                    doguDependencies.push(key);
+                }
+            }
+        }
+        for (const dependency of doguDependencies) {
+            runYarn(['up', '-R', dependency]);
+        }
+        if (script.endsWith('.ts')) {
+            runYarn(['tsc', '-b']);
+        }
     }
-    yarn(['install']);
-    yarn(['run', 'newbie:cicd']);
-    yarn(['up']);
-    yarn(['run', 'build:cicd']);
-    yarn(['ts-node', script]);
+    if (script.endsWith('.js')) {
+        runYarn(['node', script]);
+    }
+    else if (script.endsWith('.ts')) {
+        runYarn(['ts-node', script]);
+    }
+    else {
+        throw new Error(`Unexpected script extension: ${script}`);
+    }
 });
 //# sourceMappingURL=main.js.map
